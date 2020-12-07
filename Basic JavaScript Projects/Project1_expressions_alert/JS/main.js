686dec99db7080f6f409069067dbc5d6061af59f
Heather-Hanson <hjhnsn82@hotmail.com>
2020-12-07 19:56:15 -0400

    Basic JavaScript Projects Folder v1.0
    Initial commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var Dad="Ron", Mom="Petronella", Sister="Rhonda", Me="Heather"; 
+var Dad=Dad.fontcolor("brown"), Mom=Mom.fontcolor("green"), Sister=Sister.fontcolor("pink"), Me=Me.fontcolor("red");
+
+window.alert("My Family"); //Pop-up with button
+
+document.write(Dad+Mom+Sister+Me);//Will display our names written in assigned colours
+document.write("The total number of family members is:"); 
+document.write(2+2); //Two parents, plus two children
+
+
